d8d014efd464fa4b31ef962d9aceb4b90d2bc87d
christiancasey <christiancasey86@gmail.com>
2022-10-18 13:03:25 +0200

    Messing with merging front and backend

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 
 import App from './App';
+import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-      <App />
-    {/* </BrowserRouter> */}
+    <BrowserRouter>
+      <App2 />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
